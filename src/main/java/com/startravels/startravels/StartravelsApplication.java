6735f06b842569457fcfe6ba37f510c7f54ca86b
Ewelina Kania <ewelina.kania93@gmail.com>
2020-11-14 14:45:17 +0100

    Initial commit, basic configuration

---
+++ src/main/java/com/startravels/startravels/StartravelsApplication.java
@@ -0,0 +1,13 @@
+package com.startravels.startravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartravelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startravels/startravels/StartravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startravels.startravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
