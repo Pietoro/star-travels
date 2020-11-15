38a7d3cb64e270aa3ebcec3f9b5472b9dfb5147f
Ewelina Kania <ewelina.kania93@gmail.com>
2020-11-15 12:02:26 +0100

    Models - planet

---
+++ src/main/java/com/startravels/startravels/Models/Planet.java
@@ -0,0 +1,21 @@
+package com.startravels.startravels.Models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "planets")
+public class Planet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = State.class)
+    private State state;
+}
