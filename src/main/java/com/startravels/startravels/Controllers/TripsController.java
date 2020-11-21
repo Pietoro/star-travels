bef45734d528308c68ea22b423b5141ec9b45f91
Ewelina Kania <ewelina.kania93@gmail.com>
2020-11-21 15:05:24 +0100

    Serve trips searched by multiple parameters

--- src/main/java/com/startravels/startravels/Controllers/TripsController.java
+++ src/main/java/com/startravels/startravels/Controllers/TripsController.java
@@ -3,10 +3,13 @@ package com.startravels.startravels.Controllers;
 import com.startravels.startravels.Models.Trip;
 import com.startravels.startravels.Models.TripDAO;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
@@ -16,8 +19,19 @@ public class TripsController {
 
     private TripDAO tripDAO;
 
-    @GetMapping
-    public List<Trip> getTrips() {
+    @GetMapping("/promoted")
+    public List<Trip> getPromotedTrips() {
         return tripDAO.findByPromoted(true);
+    }
+
+    @GetMapping
+    public List<Trip> getTrips(
+            @RequestParam(required = false) String cityName,
+            @RequestParam(required = false) String planetName,
+            @RequestParam(required = false) @DateTimeFormat(pattern="yyyy-MM-dd")LocalDate departureDate,
+            @RequestParam(required = false) @DateTimeFormat(pattern="yyyy-MM-dd")LocalDate returnDate
+    ) {
+        return tripDAO.findBySearchParameters(
+                cityName, planetName, departureDate, returnDate);
     }
 }
