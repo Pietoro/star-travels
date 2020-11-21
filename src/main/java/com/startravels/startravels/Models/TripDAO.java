0b7cf2316e49f85d52dfff53dd9fbd33d9b43dd0
Ewelina Kania <ewelina.kania93@gmail.com>
2020-11-21 15:03:52 +0100

    Add finding by multiple search parameters method

--- src/main/java/com/startravels/startravels/Models/TripDAO.java
+++ src/main/java/com/startravels/startravels/Models/TripDAO.java
@@ -1,10 +1,32 @@
 package com.startravels.startravels.Models;
 
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TripDAO extends PagingAndSortingRepository<Trip, Integer> {
 
+
     List<Trip> findByPromoted(boolean isPromoted);
+
+    @Query("select t from Trip t " +
+            "where (:hotelCityName is null " +
+            "OR t.hotel.city.name like %:hotelCityName%) " +
+            "AND (:hotelPlanetName is null " +
+            "OR t.hotel.city.planet.name like %:hotelPlanetName%) " +
+            "AND (:departureDate is null " +
+            "OR t.departureDate >= :departureDate)" +
+            "AND (:returnDate is null " +
+            "OR t.returnDate <= :returnDate)"
+            )
+    List<Trip> findBySearchParameters(
+            @Param("hotelCityName")String hotelCityName,
+            @Param("hotelPlanetName")String hotelPlanetName,
+            @Param("departureDate") LocalDate departureDate,
+            @Param("returnDate")LocalDate returnDate
+            );
 }
