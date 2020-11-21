251277e2f55938d5f0fd2631ad3ac47b4196cf3e
Ewelina Kania <ewelina.kania93@gmail.com>
2020-11-21 13:04:07 +0100

    Basic content and styling in trip list

--- frontend/src/components/TripList.jsx
+++ frontend/src/components/TripList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState }  from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 
 export const TripList = () => {
 
@@ -13,14 +14,100 @@ export const TripList = () => {
     }
   }
 
+  const StyledContainer = styled.div`
+    margin: 1.5rem 0;
+    border: 1px solid black;
+    display: inline-block;
+    width: 95%;
+  `
+
+  const StyledUl = styled.ul`
+    list-style: none;
+  `
+
+  const StyledTripContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    border: 1px solid blue;
+    margin: 1rem;
+  `
+
+  const StyledLeftContainer = styled.div`
+
+  `
+
+  const StyledImg = styled.div`
+    background-color: black;
+    width: 15rem;
+    height: 15rem;
+  `
+
+  const StyledMiddleContainer = styled.div`
+    padding: 0.5rem;
+    text-align: left;
+    border-right: 1px solid lightgray;
+    flex-grow: 1;
+  `
+
+  const StyledRightContainer = styled.div`
+    padding-top: 0.5rem;
+    width: 20rem;
+    font-size: 1.2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  `
+
+  const StyledPriceDiv = styled.div`
+      
+  `
+
+  const StyledSeeMoreLink = styled.div`
+    background-color: lightgreen;
+    padding: 0.5rem;
+  `
+
+
+
   useEffect(() => downloadData(), [])
 
   return (
-    <div>
-      <ul>
-  {tripData.map((trip) => <li>{trip.hotel.name}, {trip.hotel.city.name}, {trip.price}</li>)}
-      </ul>
-    </div>
-
+    <StyledContainer>
+      <StyledUl>
+        {tripData.map((trip) => 
+        <li key={`trip_${trip.id}`}>
+          <StyledTripContainer>
+            <StyledLeftContainer>
+              <StyledImg></StyledImg>
+            </StyledLeftContainer>
+            <StyledMiddleContainer>
+              {trip.hotel.city.planet.name}, {trip.hotel.city.name}
+              <div>
+                {trip.hotel.name}
+              </div>
+              <div>
+                <b>From: </b>{trip.dockFrom.city.planet.name}, {trip.dockFrom.city.name}
+              </div>
+              <div>
+                {trip.boardType.code}
+              </div>
+              <div>
+                {trip.promoted ? <span>&#9733;</span> : undefined}
+              </div>
+            </StyledMiddleContainer>
+            <StyledRightContainer>
+              {trip.departureDate} - {trip.returnDate}
+              <StyledPriceDiv>
+                {trip.price}
+              </StyledPriceDiv>
+              <StyledSeeMoreLink>See more</StyledSeeMoreLink>
+            </StyledRightContainer>
+          </StyledTripContainer>
+          
+          
+          </li>)
+        }
+      </StyledUl>
+    </StyledContainer>
   )
 }
