099c669f720cc3ccdc9492f18f88f95aba6c4a2f
Ewelina Kania <ewelina.kania93@gmail.com>
2020-11-22 14:18:59 +0100

    Refactor trips list item to its own component

--- frontend/src/components/TripList.jsx
+++ frontend/src/components/TripList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState }  from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
+import { TripsListItem } from './TripsListItem';
 
 const StyledContainer = styled.div`
   margin: 1.5rem 0;
@@ -10,49 +11,6 @@ const StyledContainer = styled.div`
 
 const StyledUl = styled.ul`
   list-style: none;
-`
-
-const StyledTripContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  border: 1px solid lightgray;
-  margin: 1rem;
-`
-
-const StyledLeftContainer = styled.div`
-
-`
-
-const StyledImg = styled.div`
-  background-color: black;
-  width: 15rem;
-  height: 15rem;
-`
-
-const StyledMiddleContainer = styled.div`
-  padding: 0.5rem;
-  text-align: left;
-  border-right: 1px solid lightgray;
-  flex-grow: 1;
-`
-
-const StyledRightContainer = styled.div`
-  padding-top: 0.5rem;
-  width: 20rem;
-  font-size: 1.2rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
-
-const StyledPriceDiv = styled.div`
-    
-`
-
-const StyledSeeMoreLink = styled.div`
-  background-color: var(--primary-col);
-  color: white;
-  padding: 0.5rem;
 `
 
 const API = '/api/trips'
@@ -85,38 +43,9 @@ export const TripList = ({tripData}) => {
   return (
     <StyledContainer>
       <StyledUl>
-        {tripData.map((trip) => 
-        <li key={`trip_${trip.id}`}>
-          <StyledTripContainer>
-            <StyledLeftContainer>
-              <StyledImg></StyledImg>
-            </StyledLeftContainer>
-            <StyledMiddleContainer>
-              {trip.hotel.city.planet.name}, {trip.hotel.city.name}
-              <div>
-                {trip.hotel.name}
-              </div>
-              <div>
-                <b>From: </b>{trip.dockFrom.city.planet.name}, {trip.dockFrom.city.name}
-              </div>
-              <div>
-                {trip.boardType.code}
-              </div>
-              <div>
-                {trip.promoted ? <span>&#9733;</span> : undefined}
-              </div>
-            </StyledMiddleContainer>
-            <StyledRightContainer>
-              {trip.departureDate} - {trip.returnDate}
-              <StyledPriceDiv>
-                {trip.price}
-              </StyledPriceDiv>
-              <StyledSeeMoreLink>See more</StyledSeeMoreLink>
-            </StyledRightContainer>
-          </StyledTripContainer>
-          
-          
-          </li>)
+        {tripData.map((trip) =>
+          <TripsListItem trip={trip}/>
+        )
         }
       </StyledUl>
     </StyledContainer>
---
+++ frontend/src/components/TripsListItem.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const StyledTripContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  border: 1px solid lightgray;
+  margin: 1rem;
+`
+
+const StyledLeftContainer = styled.div`
+
+`
+
+const StyledImg = styled.div`
+  background-color: black;
+  width: 15rem;
+  height: 15rem;
+`
+
+const StyledMiddleContainer = styled.div`
+  padding: 0.5rem;
+  text-align: left;
+  border-right: 1px solid lightgray;
+  flex-grow: 1;
+`
+
+const StyledRightContainer = styled.div`
+  padding-top: 0.5rem;
+  width: 20rem;
+  font-size: 1.2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const StyledPriceDiv = styled.div`
+    
+`
+
+const StyledSeeMoreLink = styled.div`
+  background-color: var(--primary-col);
+  color: white;
+  padding: 0.5rem;
+`
+
+
+export const TripsListItem = ({trip}) => {
+  return (
+    <li key={`trip_${trip.id}`}>
+      <StyledTripContainer>
+        <StyledLeftContainer>
+          <StyledImg></StyledImg>
+        </StyledLeftContainer>
+        <StyledMiddleContainer>
+          {trip.hotel.city.planet.name}, {trip.hotel.city.name}
+          <div>
+            {trip.hotel.name}
+          </div>
+          <div>
+            <b>From: </b>{trip.dockFrom.city.planet.name}, {trip.dockFrom.city.name}
+          </div>
+          <div>
+            {trip.boardType.code}
+          </div>
+          <div>
+            {trip.promoted ? <span>&#9733;</span> : undefined}
+          </div>
+        </StyledMiddleContainer>
+        <StyledRightContainer>
+          {trip.departureDate} - {trip.returnDate}
+          <StyledPriceDiv>
+            {trip.price}
+          </StyledPriceDiv>
+          <Link to={`/trips/${trip.id}`}>
+            <StyledSeeMoreLink>See more</StyledSeeMoreLink>
+          </Link>
+        </StyledRightContainer>
+      </StyledTripContainer>
+      </li>
+  )
+}
